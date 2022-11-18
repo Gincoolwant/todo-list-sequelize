e0e61e9c362ebd47e273160a2074b4dce426abd9
Gincoolwant <soulbox790326@gmail.com>
2022-11-18 13:12:47 +0800

    project init

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+const bcrypt = require('bcryptjs')
+
+const app = express()
+
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+app.get('/', (req ,res) => {
+  res.send('router ok')
+})
+
+const PORT = 3000
+app.listen(PORT, () => {
+  console.log(`Server is connecting to http://localhost:${PORT}`)
+})
